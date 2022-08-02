bf78deb97dadcf0d0928e5b87b451007968be649
Karan <karansethia24@gmail.com>
2022-08-03 01:21:23 +0530

    LeetCode Questions

---
+++ LeetCode/ArrayConcat.java
@@ -0,0 +1,19 @@
+package com.company.LeetCode;
+
+import java.util.Arrays;
+
+public class ArrayConcat {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3};
+        int n = arr.length;
+        int[] ans = new int[2 * n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = arr[i];
+        }
+        for (int j = 0; j < n ; j++) {
+            ans[j+n] = arr[j];
+        }
+        System.out.println(Arrays.toString(ans));
+    }
+}
---
+++ LeetCode/ArrayFromPermutation.java
@@ -0,0 +1,20 @@
+package com.company.LeetCode;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayFromPermutation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nums = new int[6];
+        int n = nums.length;
+        int[] ans = new int[n];
+        for (int i = 0; i <= nums.length-1; i++) {
+            nums[i] = sc.nextInt();
+        }
+        for (int i = 0; i <= n-1; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        System.out.println(Arrays.toString(ans));
+    }
+}
