67920c096646580ef39a7946f7460cf4e6dc9d40
Karan <karansethia24@gmail.com>
2022-08-07 05:18:27 +0530

    Linear Search in Arrays

--- LS_in_2DArray.java
+++ LS_in_2DArray.java
@@ -1,2 +1,26 @@
-package com.company;public class LS_in_2DArray {
+package com.company;
+
+import java.util.Arrays;
+
+public class LS_in_2DArray {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {23,10,3,4,56},
+                {43,2,12,13,53},
+                {25,63,1,33,7}
+        };
+        int target = 943;
+        System.out.println(Arrays.toString(search(arr,target)));
+    }
+    static int[] search(int[][] arr,int target){
+        int ans = arr[0][0];
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if(arr[row][col] == target){
+                    return new int[]{row,col};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
 }
--- MInNumber.java
+++ MInNumber.java
@@ -1,2 +1,17 @@
-package com.company;public class MInNumber {
+package com.company;
+
+public class MInNumber {
+    public static void main(String[] args) {
+        int[] arr = {20,1,4,3,21,-5,24,-8};
+        System.out.println(min(arr));
+    }
+    static int min(int[] arr){
+        int minimum = arr[1];
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] < minimum){
+                minimum = arr[i];
+            }
+        }
+        return minimum;
+    }
 }
