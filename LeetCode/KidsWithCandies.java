bdb25c335f67fb1a82d91c88e245c32e61595b92
Karan <karansethia24@gmail.com>
2022-08-10 00:26:34 +0530

    Kids with Candies

---
+++ LeetCode/KidsWithCandies.java
@@ -0,0 +1,28 @@
+package com.company.LeetCode;
+
+import java.util.Arrays;
+
+public class KidsWithCandies {
+    public static void main(String[] args) {
+        int[] candies = {4,2,1,1,2};
+        int n = candies.length;
+        int extraCandies = 1;
+        boolean[] isMax = new boolean[n];
+        int maximum = candies[0];
+        for (int candy : candies) {
+            if (candy > maximum) {
+                maximum = candy;
+            }
+        }
+        for(int i = 0;i<n;i++){
+            candies[i] += extraCandies;
+            if(candies[i]>=maximum){
+                isMax[i] = true;
+            }
+            else{
+                isMax[i] = false;
+            }
+        }
+        System.out.println(Arrays.toString(isMax));
+    }
+}
