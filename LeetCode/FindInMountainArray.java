fccfbbf29de0719c0d3a03a415c7bb7aa2e8bf93
Karan <karansethia24@gmail.com>
2022-08-23 00:06:13 +0530

    Find in Mountain array - 2

--- LeetCode/FindInMountainArray.java
+++ LeetCode/FindInMountainArray.java
@@ -3,40 +3,17 @@ package com.company.LeetCode;
 public class FindInMountainArray {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,3,1};
-        int ans = searchElement(arr,1);
+        int ans = searchElement(arr,4);
         System.out.println(ans);
     }
 
     static int searchElement(int[] arr, int target) {
-        int start = 0;
-        int end = peakIndex(arr);
-        // to find whether the given array is ascending or descending
-        boolean isAsc = arr[start] < arr[end];
-
-        while (start <= end) {
-
-            int mid = start + (end - start) / 2;
-
-            if (arr[mid] == target) {
-                return mid;
-            }
-            if (isAsc) {
-                if (target < arr[mid]) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
-            } else {
-                if (target > arr[mid]) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
-            }
-
+        int peak = peakIndex(arr);
+        int firstTry = orderAgnosticBS(arr,target,0,peak);
+        if(firstTry != -1){
+            return firstTry;
         }
-        // element does not exist
-        return -1;
+        return orderAgnosticBS(arr,target,peak+1,arr.length-1);
     }
 
     static int peakIndex(int[] arr) {
@@ -58,5 +35,34 @@ public class FindInMountainArray {
             }
         }
         return start; // or end or mid since they are all pointing at the same element
+    }
+    static int orderAgnosticBS(int[] arr, int target, int start,int end){
+        // to find whether the given array is ascending or descending
+        boolean isAsc = arr[start] < arr[end];
+
+        while(start<=end) {
+
+            int mid = start + (end - start) / 2;
+
+            if(arr[mid] == target){
+                return mid;
+            }
+            if(isAsc){
+                if (target < arr[mid]){
+                    end = mid - 1;
+                } else{
+                    start = mid + 1;
+                }
+            } else {
+                if (target > arr[mid]){
+                    end = mid - 1;
+                } else{
+                    start = mid + 1;
+                }
+            }
+
+        }
+        // element does not exist
+        return -1;
     }
 }
