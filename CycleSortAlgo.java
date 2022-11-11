9e8c0990d946f06c593ce9ccc6c0c0c8934cdfd9
Karan <karansethia24@gmail.com>
2022-11-11 12:28:45 +0530

    Cyclic sort Algo and logic

--- CycleSortAlgo.java
+++ CycleSortAlgo.java
@@ -1,7 +1,27 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class CycleSortAlgo {
     public static void main(String[] args) {
-
+        int[] arr = { 3, 5, 4, 1, 2};
+        cyclicSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void cyclicSort(int[] arr){
+        int i = 0;
+        while(i < arr.length){
+            int correctIndex = arr[i] - 1;   // index = value - 1
+            if(arr[i] != arr[correctIndex]){
+                swap(arr,i,correctIndex);
+            }else{
+                i++;
+            }
+        }
+    }
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
     }
 }
