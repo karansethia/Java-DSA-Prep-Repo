ab41c63a98f01fd801721b1ded6e4a3b3642be27
Karan <karansethia24@gmail.com>
2023-12-16 18:31:39 +0530

    Starting OOP in Java

---
+++ Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Creating an array of students data by traditional methods
+        int[] rollno = new int[5];
+        int[] marks = new int[5];
+        String[] name = new String[5];
+
+        // But what if we wanted a data type where all the related data is combined rather than having a
+        // seperate data stored for each of the property
+        // Creating object from the class car
+        Car mustang = new Car("Ford Mustang GT500","V8",2,80000);
+//        mustang.seats = 1;
+//        mustang.price = 80000;
+//        mustang.engine = "V8";
+//        mustang.name = "Ford Mustang GT500";
+        System.out.println(mustang);
+        mustang.greeting();
+
+    }
+}
+// creating a class
+class Car {
+    String name;
+    String engine;
+    int seats;
+    double price;
+    // constructor
+    Car(String name,String engine,int seats,int price){
+        this.name = name;
+        this.engine = engine;
+        this.seats = seats;
+        this.price = price;
+    }
+    void greeting(){
+        System.out.println("The name of the car is "+this.name);
+    }
+}
